bb8ec2e7c4aceb1dc8c2183f4e73313d229c14e7
TerryHuangchungyo <0551076@nkust.edu.tw>
2020-09-25 01:20:27 +0800

    Add tutorial2

--- demo2/server/www/database.php
+++ demo2/server/www/database.php
@@ -1,9 +1,7 @@
 <?php
-    //因為使用了links，所以在apache這個容器中可以直接用"mysql"字元連線資料庫，
-    //使用links後其實就是在apache這個容器的hosts檔案中添加了"mymysql"字元到mysql容器的ip的對映
     $link = mysqli_connect("db", "root", "root");
     if(!$link)
-        die('Could not connect: ' . mysqli_error());
+        die('連接失敗' . mysqli_error());
     else
         echo "成功連上了我好興奮啊!!";
     mysqli_close($link);
